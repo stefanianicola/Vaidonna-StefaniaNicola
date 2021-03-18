703d92aa8ad58e6fea4f1dedda7fda2c0a649a4e
stefanianicola <58124232+stefanianicola@users.noreply.github.com>
2021-03-18 00:12:08 -0300

    Entrega 1

---
+++ vaidonna-sn/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Vaidonna</h1>
+      <p>Todo para tu mascota</p>
+    </div>
+  );
+}
+
+export default App;
